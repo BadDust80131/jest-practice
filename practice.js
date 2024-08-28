b6ad2136447d8efaa93b436ea7101cd96def212a
BadDust80131 <baddust80131@gmail.com>
2024-08-28 13:10:56 -0700

    calc done

--- practice.js
+++ practice.js
@@ -6,4 +6,19 @@ function reverseString(string) {
     return string.split("").reverse().join("");
 }
 
-module.exports = { capitalize, reverseString };
+class calculator {
+    add(a,b) {
+        return a + b;
+    }
+    subtract(a,b) {
+        return a - b;
+    }
+    divide(a,b) {
+        return a / b;
+    }
+    multiply(a,b) {
+        return a * b;
+    }
+}
+const calc = new calculator()
+module.exports = { capitalize, reverseString, calc };
--- practice.test.js
+++ practice.test.js
@@ -7,3 +7,19 @@ test("'hello' returns 'Hello'", () => {
 test("'hello' return 'olleh'", () => {
     expect(practice.reverseString("hello")).toBe("olleh");
 })
+
+test("1 + 1 is 2", () => {
+    expect(practice.calc.add(1,1)).toBe(2);
+})
+
+test("2 - 1 is 1", () => {
+    expect(practice.calc.subtract(2,1)).toBe(1);
+})
+
+test("2 * 2 is 4", () => {
+    expect(practice.calc.multiply(2,2)).toBe(4);
+})
+
+test("2 / 2 is 1", () => {
+    expect(practice.calc.divide(2,2)).toBe(1);
+})
